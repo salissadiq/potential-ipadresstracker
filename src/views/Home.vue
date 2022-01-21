d7415774945e4ab422fe21378758e3fb5e4ef094
salissadiq <salissadiq@gmail.com>
2022-01-21 08:57:00 +0100

    Add functionality to get client's Ip by defualt if the search input is empty

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -52,7 +52,7 @@ export default {
     const queryIp = ref("")
     const ipInfo = ref(null)
     onMounted(()=>{
-      myMap = leaflet.map('mapid').setView([51.505, -0.09], 13);
+      myMap = leaflet.map('mapid').setView([13.005873, 5.247552], 9);
       leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2Fsc2NvZGVzIiwiYSI6ImNreW8wMTc1NTJtbzEyb3AwODZpYjhhZDgifQ.i42mc7jKGhBqh-GvcAvXRA', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -65,7 +65,14 @@ export default {
 
     const getIpInfo = async () => {
       try {
-        const data = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_FHmifZsdVxhJVx5It5qODuhy9QvyB&ipAddress=${queryIp.value}`)
+        const clientIp = await axios.get(`https://api.ipify.org/?format=json`)
+        const clientIpResult = clientIp.data
+        let data
+          if(!queryIp.value) {
+            data = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_FHmifZsdVxhJVx5It5qODuhy9QvyB&ipAddress=${clientIpResult.ip}`)
+          }else{
+            data = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_FHmifZsdVxhJVx5It5qODuhy9QvyB&ipAddress=${queryIp.value}`)
+          }
         const result = data.data
         ipInfo.value = {
           address: result.ip,
@@ -76,6 +83,8 @@ export default {
           lat: result.location.lat,
           lng: result.location.lng
         }
+        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(myMap)
+        myMap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
       } catch(err) {
         alert(err.message)
       }
